7367ea261a79379f2faf6c533ad3fb24766c41dd
propspero238 <keithc238@gmail.com>
2016-08-15 09:37:31 -0700

    refactored for clarity

--- src/test/java/org/jenkinsci/plugins/liquibase/integration/RollbackResultTest.java
+++ src/test/java/org/jenkinsci/plugins/liquibase/integration/RollbackResultTest.java
@@ -67,10 +67,12 @@ public class RollbackResultTest {
     public void should_report_success_with_successful_rollbacks()
             throws IOException, ExecutionException, InterruptedException, SQLException, LiquibaseException {
 
-        final int numberOfChangesetsToRollback = 2;
-        FreeStyleBuild build =
-                createAndLaunchRollbackJob(sunnyDayChangeset, configureRollbackCount(numberOfChangesetsToRollback));
-        RolledbackChangesetAction action = build.getAction(RolledbackChangesetAction.class);
+        createDatabase(sunnyDayChangeset);
+
+        RollbackBuildStep buildStep = createBaseBuildStep(RollbackBuildStep.RollbackStrategy.COUNT, sunnyDayChangeset);
+        int numberOfChangesetsToRollback = 2;
+        buildStep.setNumberOfChangesetsToRollback(numberOfChangesetsToRollback);
+        RolledbackChangesetAction action = launchBuild(buildStep);
 
         assertThat(action.getBuild().getResult(), is(Result.SUCCESS));
         assertThat(action.getRolledbackChangesets(), hasSize(numberOfChangesetsToRollback));
@@ -80,11 +82,13 @@ public class RollbackResultTest {
     public void should_rollback_using_tag_sucessfully()
             throws IOException, SQLException, LiquibaseException, ExecutionException, InterruptedException {
 
-        FreeStyleBuild build = createAndLaunchRollbackJob(sunnyDayChangeset, configureToRollbackToFirstTag());
+        createDatabase(sunnyDayChangeset);
+        RollbackBuildStep rollbackBuildStep = createBaseBuildStep(RollbackBuildStep.RollbackStrategy.TAG, sunnyDayChangeset );
+        rollbackBuildStep.setRollbackToTag(FIRST_TAG);
 
-        assertThat(build.getResult(), is(Result.SUCCESS));
+        RolledbackChangesetAction action = launchBuild(rollbackBuildStep);
 
-        RolledbackChangesetAction action = build.getAction(RolledbackChangesetAction.class);
+        assertThat(action.getBuild().getResult(), is(Result.SUCCESS));
         int amountOfChangesetsExistingAfterTag = 2;
         assertThat(action.getRolledbackChangesets(), hasSize(amountOfChangesetsExistingAfterTag));
     }
@@ -92,12 +96,17 @@ public class RollbackResultTest {
     @Test
     public void should_rollback_according_to_date()
             throws IOException, SQLException, LiquibaseException, ExecutionException, InterruptedException {
-        FreeStyleBuild build = createAndLaunchRollbackJob(sunnyDayChangeset, configureToRollbackAllChangesetsAppliedAfterInception());
-        RolledbackChangesetAction action = build.getAction(RolledbackChangesetAction.class);
+        createDatabase(sunnyDayChangeset);
+        RollbackBuildStep rollbackBuildStep =
+                createBaseBuildStep(RollbackBuildStep.RollbackStrategy.DATE, sunnyDayChangeset);
+        Date yesterday = dateBeforeChangesetsApplied();
+        rollbackBuildStep.setRollbackToDate(new SimpleDateFormat(RollbackBuildStep.DATE_PATTERN).format(yesterday));
+
+        RolledbackChangesetAction resultAction = launchBuild(rollbackBuildStep);
 
         int totalNumberOfChangesets = 4;
 
-        assertThat(action.getRolledbackChangesets(), hasSize(totalNumberOfChangesets));
+        assertThat(resultAction.getRolledbackChangesets(), hasSize(totalNumberOfChangesets));
     }
 
     @Test
@@ -107,47 +116,18 @@ public class RollbackResultTest {
         File changesetContainingError = LiquibaseTestUtil
                 .createProjectFile(temporaryFolder, CHANGELOG_WITH_ROLLBACK_ERROR_RESOURCE_PATH);
 
-        FreeStyleBuild build =
-                createAndLaunchRollbackJob(changesetContainingError, configureRollbackCount(2));
+        createDatabase(changesetContainingError);
 
-        RolledbackChangesetAction action = build.getAction(RolledbackChangesetAction.class);
+        RollbackBuildStep buildStep =
+                createBaseBuildStep(RollbackBuildStep.RollbackStrategy.COUNT, changesetContainingError);
+        buildStep.setNumberOfChangesetsToRollback(2);
 
-        assertThat(action, notNullValue());
-        assertThat(build.getResult(), is(Result.UNSTABLE));
+        RolledbackChangesetAction resultAction = launchBuild(buildStep);
+        assertThat(resultAction, notNullValue());
+        assertThat(resultAction.getBuild().getResult(), is(Result.UNSTABLE));
     }
 
-    protected FreeStyleBuild createAndLaunchRollbackJob(File changesetFile,
-                                                        RollStepConfigurer rollStepConfigurer)
-            throws InterruptedException, ExecutionException, LiquibaseException, SQLException, IOException {
-        createDatabaseUsingLiquibase(changesetFile);
-        RollbackBuildStep rollbackBuildStep = createAndConfigureBuildStep(changesetFile, rollStepConfigurer);
-        return addStepAndLaunchBuild(rollbackBuildStep);
-    }
-
-    protected static Date composeYesterdaysDate() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, -1);
-        return calendar.getTime();
-    }
-
-
-    protected FreeStyleBuild addStepAndLaunchBuild(RollbackBuildStep rollbackBuildStep)
-            throws InterruptedException, ExecutionException {
-        project.getBuildersList().add(rollbackBuildStep);
-        return project.scheduleBuild2(0).get();
-    }
-
-    protected RollbackBuildStep createAndConfigureBuildStep(File changelogFile, RollStepConfigurer rollStepConfigurer) {
-        RollbackBuildStep rollbackBuildStep = new RollbackBuildStep();
-        rollbackBuildStep.setChangeLogFile(changelogFile.getAbsolutePath());
-        rollbackBuildStep.setUrl(dbUrl);
-        rollbackBuildStep.setDatabaseEngine("H2");
-        rollStepConfigurer.configure(rollbackBuildStep);
-
-        return rollbackBuildStep;
-    }
-
-    protected void createDatabaseUsingLiquibase(File changeset) throws IOException, SQLException, LiquibaseException {
+    protected void createDatabase(File changeset) throws IOException, SQLException, LiquibaseException {
         File inmemoryDatabaseFile = temporaryFolder.newFile();
         InputStream asStream = getClass().getResourceAsStream("/example-changesets/unit-test.h2.liquibase.properties");
         Properties liquibaseProperties = new Properties();
@@ -158,61 +138,38 @@ public class RollbackResultTest {
         JdbcConnection jdbcConnection = new JdbcConnection(connection);
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
 
-        ClassLoaderResourceAccessor classLoaderResourceAccessor = new ClassLoaderResourceAccessor(ClassLoader.getSystemClassLoader());
+        ClassLoaderResourceAccessor classLoaderResourceAccessor =
+                new ClassLoaderResourceAccessor(ClassLoader.getSystemClassLoader());
         CompositeResourceAccessor resourceAccessor = new CompositeResourceAccessor(new FileSystemResourceAccessor(),
                 classLoaderResourceAccessor);
         Liquibase liquibase = new Liquibase(changeset.getAbsolutePath(), resourceAccessor, database);
         liquibase.update(new Contexts());
     }
 
-    static RollbackToFirstTag configureToRollbackToFirstTag() {
-        return new RollbackToFirstTag();
+    protected RollbackBuildStep createBaseBuildStep(RollbackBuildStep.RollbackStrategy rollbackStrategy,
+                                                    File changelogFile) {
+        RollbackBuildStep buildStep1 = new RollbackBuildStep();
+        buildStep1.setChangeLogFile(changelogFile.getAbsolutePath());
+        buildStep1.setUrl(dbUrl);
+        buildStep1.setDatabaseEngine("H2");
+        RollbackBuildStep buildStep = buildStep1;
+        buildStep.setRollbackType(rollbackStrategy.name());
+        return buildStep;
     }
 
-    protected static RollbackCount configureRollbackCount(int numberOfChangesetsToRollback) {
-        return new RollbackCount(numberOfChangesetsToRollback);
+    protected RolledbackChangesetAction launchBuild(RollbackBuildStep buildStep)
+            throws InterruptedException, ExecutionException {
+        project.getBuildersList().add(buildStep);
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        return build.getAction(RolledbackChangesetAction.class);
     }
 
-    static RollbackToYesterday configureToRollbackAllChangesetsAppliedAfterInception() {
-        return new RollbackToYesterday();
+
+    protected static Date dateBeforeChangesetsApplied() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -1);
+        return calendar.getTime();
     }
 
 
-    private static class RollbackToYesterday implements RollStepConfigurer {
-
-        @Override
-        public void configure(RollbackBuildStep rollbackBuildStep) {
-            rollbackBuildStep.setRollbackType(RollbackBuildStep.RollbackStrategy.DATE.name());
-            Date yesterday = composeYesterdaysDate();
-            rollbackBuildStep
-                    .setRollbackToDate(new SimpleDateFormat(RollbackBuildStep.DATE_PATTERN).format(yesterday));
-        }
-    }
-
-    private static class RollbackToFirstTag implements RollStepConfigurer {
-        @Override
-        public void configure(RollbackBuildStep rollbackBuildStep) {
-            rollbackBuildStep.setRollbackType(RollbackBuildStep.RollbackStrategy.TAG.name());
-            rollbackBuildStep.setRollbackToTag(FIRST_TAG);
-        }
-    }
-
-    private static class RollbackCount implements RollStepConfigurer {
-        private final int numberOfChangesetsToRollback;
-
-        public RollbackCount(int numberOfChangesetsToRollback) {
-            this.numberOfChangesetsToRollback = numberOfChangesetsToRollback;
-        }
-
-        @Override
-        public void configure(RollbackBuildStep rollbackBuildStep) {
-            rollbackBuildStep.setRollbackType(RollbackBuildStep.RollbackStrategy.COUNT.name());
-            rollbackBuildStep.setNumberOfChangesetsToRollback(numberOfChangesetsToRollback);
-
-        }
-    }
-
-    interface RollStepConfigurer {
-        void configure(RollbackBuildStep rollbackBuildStep);
-    }
 }
